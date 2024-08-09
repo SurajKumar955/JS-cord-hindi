b35bc572b95c9478a1710ccede5dbe3a06c2428c
suraj kumar <surk05032000@gmail.com>
2024-08-09 21:40:18 +0000

    conversionoperation

---
+++ o1_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+/*
+output conversion
+null -> 0
+undefined -> 1
+"22ascds" -> NaN
+true -> 1; false -> 0
+""-> false
+isLoggedIn = 1 -> true
+isLoggedIn = "Suraj" -> true
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
