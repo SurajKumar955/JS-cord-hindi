6899d026617d41682a7f7ceafe4082e0a7a254aa
suraj kumar <surk05032000@gmail.com>
2024-08-09 20:49:41 +0000

    datatype

---
+++ o1_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js cord as newer version
+/*
+alert(statement) in used operater but alert(3 + 3) we are using nodejs, not browser
+alert in used deferent centaxt 
+*/
+console.log(3 
+    + 3) // code readbility should be high
+    console.log("Suraj");
+
+    let name = "Suraj" //string datatype
+    let age = 21 // number "
+    let isloggedIn = false //boolean
+
+    // primitive data type 
+    /*datatype:-> 1:-number => 2 to power 53 range;
+    2:-bigint; 3:-string = ""; 4:-boolean = true/false; 5:-null is standalone value;
+    6:- undefined = not value assign; 7:- symbol = unique;
+*/  
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
