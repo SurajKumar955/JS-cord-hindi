77fe808032d589fee313c627a66033897a6edc8e
suraj kumar <surk05032000@gmail.com>
2024-08-09 19:25:31 +0000

    container config done

---
+++ o1_basics/01_variable.js
@@ -0,0 +1,20 @@
+const account_Id = 12367
+let account_Email ="surajkumar02032003@gmail.com"
+var account_password ="7667798" 
+account_City ="Durgapur"
+let account_state; // Undefined
+
+// account_Id = 3 // not allowed
+account_Email ="suraj7280805994@gmail.com"
+account_password ="sur7aj6"
+account_City = "munger"
+
+console.log(account_Id);
+
+/*
+prefer not to use var
+because of issus in block scope and functional scope
+*/
+
+console.table([account_Id, account_state, account_Email, account_password,account_City]);
+
