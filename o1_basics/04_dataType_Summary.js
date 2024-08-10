f03fad231835c0b71f3e72593103718e97716f70
suraj kumar <surk05032000@gmail.com>
2024-08-10 19:47:32 +0000

    datatype

--- o1_basics/04_dataType_Summary.js
+++ o1_basics/04_dataType_Summary.js
@@ -30,3 +30,30 @@ const myFunction = function(){
 }
 
 console.log(typeof bignumber);
+
+// https://tc39.es/ecma262/
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//  ++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (primitive), Heap (Non-Primitive)
+
+let myInstagramname = "suraj_kumar_955"
+
+let anothername = "myInstagramname"
+anothername = "surajname"
+
+console.log(myInstagramname);
+console.log(anothername);
+
+let userOne = {
+    email: "suraj7280805994@gmail.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "suraj7280805994@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
