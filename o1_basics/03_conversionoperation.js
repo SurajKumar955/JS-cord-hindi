e1d92481f8f89b78042e10ea23b65f71afd6b083
suraj kumar <surk05032000@gmail.com>
2024-08-10 08:09:32 +0000

    conversionoperation

--- o1_basics/03_conversionoperation.js
+++ o1_basics/03_conversionoperation.js
@@ -1,9 +1,9 @@
-let score = null
+// let score = null
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let ValueInNumber = Number(score)
+// let ValueInNumber = Number(score)
 
 // console.log(typeof ValueInNumber);
 // console.log(ValueInNumber);
@@ -19,13 +19,42 @@ isLoggedIn = 1 -> true
 isLoggedIn = "Suraj" -> true
 */
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 22
+// let someNumber = 22
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***********************Operations************************
+let value = 44
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Suraj"
+let str3 = str1 + str2
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+//console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
