d448577eb1808a556d2f9473a9ebf02727959a05
suraj kumar <surk05032000@gmail.com>
2024-08-10 19:17:58 +0000

    datatype

---
+++ o1_basics/04_dataType_Summary.js
@@ -0,0 +1,32 @@
+//primitive
+// 7 type:- String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedId = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+// const bigNumber = 837246983253289n
+
+//Reference(Non- Primitive)
+// Array, Object, Function
+
+
+const navi = ["Ram", "Mohan", "Rekha",]
+let myObj = {
+    name: "Suraj",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bignumber);
